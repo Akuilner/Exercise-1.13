f9018035d3b2fdecfb4ba75a056562be6ffe54fe
Akuilner <och5@comcast.net>
2017-05-07 23:51:58 -0400

    Exercise 1.13
    From the textbook

---
+++ src/Exercise1_13.java
@@ -0,0 +1,11 @@
+
+public class Exercise1_13 {
+
+	public static void main(String[] args) {
+		System.out.println("X = " + (44.5 * 0.55 - 50.2 * 5.9) / (3.4 * 0.55 - 50.2 * 2.1));
+		System.out.println("Y = " + (3.4 * 3.9 - 44.5 * 2.1) / (3.4 * 0.55 - 50.2 * 2.1));
+		
+		
+	}
+
+}
